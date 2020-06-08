8158ea6f8d8dd8cf886a19aa28c45850f7114386
Rahul Shetty <rahul.shetty@here.com>
2020-06-08 06:32:21 +0530

    Add react navigation module

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,25 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 const Routes = () => (
-  <View>
-    <Text>Routes</Text>
-  </View>
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
 );
 
 export default Routes;
